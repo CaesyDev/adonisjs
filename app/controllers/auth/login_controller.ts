8eefe5907d2b5a585aa7bdb729f0150282a998f2
Okafor Kenechukwu Howell <62034993+CaesyDev@users.noreply.github.com>
2025-01-03 15:38:29 +0100

    remember me session guard

--- app/controllers/auth/login_controller.ts
+++ app/controllers/auth/login_controller.ts
@@ -9,10 +9,10 @@ export default class LoginController {
   }
   
   async store({request, response, auth}: HttpContext) {
-    const {email, password} = await request.validateUsing(loginValidator);
+    const {email, password,isRememberMe} = await request.validateUsing(loginValidator);
     const user = await User.verifyCredentials(email, password);
     //create session
-    await auth.use("web").login(user)
+    await auth.use("web").login(user, isRememberMe)
     return response.redirect().toPath("/");
   }
   
